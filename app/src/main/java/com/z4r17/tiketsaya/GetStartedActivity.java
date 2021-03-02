ccd01582ff54a663bb78fd93729e0db2d714dfaa
zenpardosi <zenfany54@gmail.com>
2021-03-02 15:08:08 +0700

    push

--- app/src/main/java/com/z4r17/tiketsaya/GetStartedActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/GetStartedActivity.java
@@ -9,15 +9,15 @@ import android.widget.Button;
 
 public class GetStartedActivity extends AppCompatActivity {
 
-    Button btn_sign_in;
-
-
+    Button btn_sign_in, btn_new_account_create;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_new_account_create = findViewById(R.id.btn_new_account_create);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +28,12 @@ public class GetStartedActivity extends AppCompatActivity {
             }
         });
 
+        btn_new_account_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoregisterone = new Intent(GetStartedActivity.this, RegisterOneActivity.class);
+                startActivity(gotoregisterone);
+            }
+        });
     }
 }
