dff385a515b08c3d36573e96ddda066d32069d7b
zenpardosi <zenfany54@gmail.com>
2021-03-02 23:06:38 +0700

    added: - plus button - back button

---
+++ app/src/main/java/com/z4r17/tiketsaya/RegisterTwoActivity.java
@@ -0,0 +1,28 @@
+package com.z4r17.tiketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class RegisterTwoActivity extends AppCompatActivity {
+
+    LinearLayout btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_two);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backtoprev = new Intent(RegisterTwoActivity.this, RegisterOneActivity.class);
+                startActivity(backtoprev);
+            }
+        });
+    }
+}
