7fb90c310a3b54b5692e8d2c64b78792b72354b1
zenpardosi <zenfany54@gmail.com>
2021-03-04 16:21:16 +0700

    push

--- app/src/main/java/com/z4r17/tiketsaya/SignInActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/SignInActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
+    Button btn_sign_in;
     TextView btn_new_account;
 
     @Override
@@ -16,7 +18,16 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_new_account = findViewById(R.id.btn_new_account);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotohome = new Intent(SignInActivity.this,HomeActivity.class);
+                startActivity(gotohome);
+            }
+        });
 
         btn_new_account.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/z4r17/tiketsaya/SuccesRegisterActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/SuccesRegisterActivity.java
@@ -2,13 +2,26 @@ package com.z4r17.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SuccesRegisterActivity extends AppCompatActivity {
 
+    Button btn_explore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_succes_register);
+
+        btn_explore = findViewById(R.id.btn_explore);
+        btn_explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotohome = new Intent(SuccesRegisterActivity.this,HomeActivity.class);
+                startActivity(gotohome);
+            }
+        });
     }
 }
