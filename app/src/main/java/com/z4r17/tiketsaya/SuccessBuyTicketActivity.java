8d343f3efd8a7403d3d54f993a67cf228d3bac0a
zenpardosi <zenfany54@gmail.com>
2021-03-07 00:01:12 +0700

    screen checkout finish v.2

---
+++ app/src/main/java/com/z4r17/tiketsaya/SuccessBuyTicketActivity.java
@@ -0,0 +1,20 @@
+package com.z4r17.tiketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SuccessBuyTicketActivity extends AppCompatActivity {
+
+    Button btn_view_ticket;
+    Button btn_my_dashboard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_buy_ticket);
+
+
+    }
+}
--- app/src/main/java/com/z4r17/tiketsaya/TicketCheckoutActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/TicketCheckoutActivity.java
@@ -2,13 +2,26 @@ package com.z4r17.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketCheckoutActivity extends AppCompatActivity {
 
+    Button btn_buy_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+
+        btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+        btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToSuccessTicket = new Intent(TicketCheckoutActivity.this, SuccessBuyTicketActivity.class);
+                startActivity(goToSuccessTicket);
+            }
+        });
     }
 }
