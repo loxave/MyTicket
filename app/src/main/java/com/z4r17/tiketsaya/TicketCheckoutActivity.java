c89abb3df8a37393a805e753db95b1b811f6cf7b
zenpardosi <zenfany54@gmail.com>
2021-03-07 00:12:48 +0700

    screen checkout finish v.3

--- app/src/main/java/com/z4r17/tiketsaya/TicketCheckoutActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/TicketCheckoutActivity.java
@@ -6,14 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class TicketCheckoutActivity extends AppCompatActivity {
 
     Button btn_buy_ticket;
+    LinearLayout btn_back_to_dashboard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+
+        btn_back_to_dashboard = findViewById(R.id.btn_back_to_dashboard);
+        btn_back_to_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backToDashboard = new Intent(TicketCheckoutActivity.this, HomeActivity.class);
+                startActivity(backToDashboard);
+            }
+        });
 
         btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
         btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
