556eb86e340a498c07810a09871e229684a7ac78
zenpardosi <zenfany54@gmail.com>
2021-03-06 16:39:19 +0700

    push

---
+++ app/src/main/java/com/z4r17/tiketsaya/TicketCheckoutActivity.java
@@ -0,0 +1,14 @@
+package com.z4r17.tiketsaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketCheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_checkout);
+    }
+}
