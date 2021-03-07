0597a0186ca5abf40cb820fd4af95ee01f517757
zenpardosi <zenfany54@gmail.com>
2021-03-07 22:20:05 +0700

    import UI Library + new photo profil

--- app/src/main/java/com/z4r17/tiketsaya/HomeActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/HomeActivity.java
@@ -7,16 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.github.florent37.shapeofview.shapes.CircleView;
+
 public class HomeActivity extends AppCompatActivity {
 
     LinearLayout btn_ticket_pisa;
+    CircleView btn_to_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        btn_to_profile = findViewById(R.id.btn_to_profile);
         btn_ticket_pisa = findViewById(R.id.btn_ticket_pisa);
+
+        btn_to_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToProfile = new Intent(HomeActivity.this,MyProfileActivity.class);
+                startActivity(goToProfile);
+            }
+        });
         btn_ticket_pisa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/z4r17/tiketsaya/MyProfileActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/MyProfileActivity.java
@@ -2,13 +2,27 @@ package com.z4r17.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    Button btn_back_to_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        btn_back_to_home = findViewById(R.id.btn_back_to_home);
+        btn_back_to_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToHome =  new Intent(MyProfileActivity.this, HomeActivity.class);
+                startActivity(goToHome);
+            }
+        });
     }
 }
