cce9855ded5f2ea500bac91ffed1b5e50650f0ba
zenpardosi <zenfany54@gmail.com>
2021-03-08 23:02:36 +0700

    initial commit

--- app/src/main/java/com/z4r17/tiketsaya/EditProfileActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/EditProfileActivity.java
@@ -2,13 +2,23 @@ package com.z4r17.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    LinearLayout btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(view -> {
+            Intent backToMyProfile = new Intent(EditProfileActivity.this, MyProfileActivity.class);
+            startActivity(backToMyProfile);
+        });
     }
 }
