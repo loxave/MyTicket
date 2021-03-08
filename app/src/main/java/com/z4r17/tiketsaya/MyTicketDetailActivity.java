cf97a51520ee1b96ffb1a1e96e47dcdd091febae
zenpardosi <zenfany54@gmail.com>
2021-03-08 16:56:46 +0700

    btn previously ticket details

--- app/src/main/java/com/z4r17/tiketsaya/MyTicketDetailActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/MyTicketDetailActivity.java
@@ -2,13 +2,28 @@ package com.z4r17.tiketsaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MyTicketDetailActivity extends AppCompatActivity {
+
+    LinearLayout btn_back_to_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_ticket_detail);
+
+        btn_back_to_edit_profile = findViewById(R.id.btn_back_to_edit_profile);
+        btn_back_to_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent prevEditProfile = new Intent(MyTicketDetailActivity.this, MyProfileActivity.class);
+                startActivity(prevEditProfile);
+            }
+        });
+
     }
 }
