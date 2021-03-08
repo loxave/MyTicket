db22a77bb6e077258d6726374a47b31f11b81598
zenpardosi <zenfany54@gmail.com>
2021-03-08 16:47:17 +0700

    my ticket detail

--- app/src/main/java/com/z4r17/tiketsaya/MyProfileActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/MyProfileActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MyProfileActivity extends AppCompatActivity {
 
     Button btn_back_to_home;
+    LinearLayout item_my_ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,22 @@ public class MyProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_profile);
 
         btn_back_to_home = findViewById(R.id.btn_back_to_home);
+
+        item_my_ticket = findViewById(R.id.item_my_ticket);
+
         btn_back_to_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent goToHome =  new Intent(MyProfileActivity.this, HomeActivity.class);
                 startActivity(goToHome);
+            }
+        });
+
+        item_my_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToMyTickets = new Intent(MyProfileActivity.this, MyTicketDetailActivity.class);
+                startActivity(goToMyTickets);
             }
         });
     }
