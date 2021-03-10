6d63463f25ec275a20fa2eeec8e3eaea8a363528
zenpardosi <zenfany54@gmail.com>
2021-03-10 15:13:25 +0700

    success buy ticket screen finish

--- app/src/main/java/com/z4r17/tiketsaya/SuccessBuyTicketActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/SuccessBuyTicketActivity.java
@@ -4,19 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SuccessBuyTicketActivity extends AppCompatActivity {
 
-    Button btn_view_ticket;
-    Button btn_my_dashboard;
+    Button btn_view_ticket, btn_my_dashboard;
+    Animation app_splash, bottomtothetop, toptothebottom;
+    TextView app_subtitle, app_title;
+    ImageView icon_success_ticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_buy_ticket);
 
+        btn_view_ticket.findViewById(R.id.btn_view_ticket);
         btn_my_dashboard = findViewById(R.id.btn_my_dashboard);
+        app_subtitle = findViewById(R.id.app_subtitle);
+        app_title = findViewById(R.id.app_title);
+        icon_success_ticket = findViewById(R.id.icon_success_ticket);
+
+        // load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        bottomtothetop = AnimationUtils.loadAnimation(this, R.anim.bottomtothetop);
+        toptothebottom = AnimationUtils.loadAnimation(this, R.anim.toptothebottom);
+        
+        // run animation
+        btn_view_ticket.startAnimation(bottomtothetop);
+        btn_my_dashboard.startAnimation(bottomtothetop);
+
+        icon_success_ticket.startAnimation(app_splash);
+
+        app_title.startAnimation(toptothebottom);
+        app_subtitle.startAnimation(toptothebottom);
+
         btn_my_dashboard.setOnClickListener(view -> {
 
             Intent backToDashboard = new Intent(SuccessBuyTicketActivity.this, HomeActivity.class);
