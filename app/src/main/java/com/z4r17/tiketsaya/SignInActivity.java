5d9a519a209964fa4cd8af26144d5d267669b6d0
zenpardosi <zenfany54@gmail.com>
2021-03-17 19:53:44 +0700

    Initial commit

--- app/src/main/java/com/z4r17/tiketsaya/SignInActivity.java
+++ app/src/main/java/com/z4r17/tiketsaya/SignInActivity.java
@@ -3,14 +3,26 @@ package com.z4r17.tiketsaya;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignInActivity extends AppCompatActivity {
 
     Button btn_sign_in;
     TextView btn_new_account;
+    EditText xusername, xpassword;
+
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +31,37 @@ public class SignInActivity extends AppCompatActivity {
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
         btn_new_account = findViewById(R.id.btn_new_account);
+        xusername = findViewById(R.id.xusername);
+        xpassword = findViewById(R.id.xpassword);
 
         btn_sign_in.setOnClickListener(view -> {
-            Intent goToHome = new Intent(SignInActivity.this, HomeActivity.class);
-            startActivity(goToHome);
+
+           final String username = xusername.getText().toString();
+            String password = xpassword.getText().toString();
+
+            reference  = FirebaseDatabase.getInstance().getReference().child("Users")
+                    .child(username);
+
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                     if(snapshot.exists()){
+                         Toast.makeText(getApplicationContext(), "Username ada", Toast.LENGTH_SHORT).show();
+
+                         //berpindah activity
+                         Intent goToHome = new Intent(SignInActivity.this, HomeActivity.class);
+                         startActivity(goToHome);
+                     }
+                    else {
+                         Toast.makeText(getApplicationContext(), "Username tidak ada!", Toast.LENGTH_SHORT).show();
+                     }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(getApplicationContext(), "Database Error", Toast.LENGTH_SHORT).show();
+                }
+            });
+
         });
 
         btn_new_account.setOnClickListener(view -> {
